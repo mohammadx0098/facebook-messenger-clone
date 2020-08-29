89da73fa47ce7038a43d3f0317d6dbe800b89397
mohammadx0098 <mohammadx0098@gmail.com>
2020-08-30 00:43:36 +0430

    last commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React , {useState , useEffect} from 'react';
 import './App.css';
-import { Button , FormControl, InputLabel, Input} from '@material-ui/core';
+import {IconButton, Button , FormControl, InputLabel, Input} from '@material-ui/core';
 import Message from './Message';
 import { db } from './firebase';
 import firebase from 'firebase';
 import FlipMove from 'react-flip-move';
+import SendIcon from '@material-ui/icons/Send';
 
 function App() {
 
@@ -18,7 +19,7 @@ function App() {
 
   useEffect(() => {
     // runs when the app component loads.
-    db.collection('messages').orderBy('timestamp','asc').onSnapshot(snapshot => {
+    db.collection('messages').orderBy('timestamp','desc').onSnapshot(snapshot => {
       
       setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})))
     })
@@ -45,13 +46,15 @@ function App() {
 
   return (
     <div className="App">
+      <img src= "https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"/>
       <h1>Hello {username}</h1>
-      <h2>welcome to the messenger.</h2>
-      <form>
-        <FormControl>
-          <InputLabel>Hello</InputLabel>
-          <Input value={input} onChange={event => setInput(event.target.value)} />    
-          <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Click me</Button>      
+      <h2>welcome to the messenger</h2>
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input className="app__input" placeholder="Enter a message ..." value={input} onChange={event => setInput(event.target.value)} />     
+          <IconButton className="app__iconButton" disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>
+            <SendIcon/>
+          </IconButton>
         </FormControl>
       </form>
 
